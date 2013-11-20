edac8c3723486ff32bdedcb4802a286eef475215
Tristan Burch <me@tristanburch.com>
2013-11-20 11:54:34 -0700

    checkpoint

--- src/main/java/com/lowtuna/gymclasscal/business/ClubManager.java
+++ src/main/java/com/lowtuna/gymclasscal/business/ClubManager.java
@@ -1,5 +1,13 @@
 package com.lowtuna.gymclasscal.business;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ClubManager {
+    private final TwentyFourHourParser parser;
+
+    public ClubManager(TwentyFourHourParser parser) {
+        this.parser = parser;
+    }
 
 }
